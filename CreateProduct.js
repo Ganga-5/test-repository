011404dc4c13b7fff924a231a51bf99aa14a6cff
Ganga-5 <105265133+Ganga-5@users.noreply.github.com>
2023-10-09 13:52:18 -0500

    Add files via upload

---
+++ CreateProduct.js
@@ -0,0 +1,15 @@
+import ProductForm from "./ProductForm";
+function CreatProduct(){
+    return (
+        <div className="row">
+            <div className="col-lg-8 mx-auto" style={{backgroundColor: 'whitesmoke', padding: '10px 20px', marginBottom: '20px'}}>
+            <ProductForm></ProductForm>
+
+            </div>
+        </div>
+    )
+    
+};
+
+export default CreatProduct;
+//<ProductForm></ProductForm>
